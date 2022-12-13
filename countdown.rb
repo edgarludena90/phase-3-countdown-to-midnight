69914fa234cf08d0073e721b6b09b1e49f82b431
edgar ludena <edgar.ludena90@gmail.com>
2022-12-13 08:32:34 -0500

    commit tree de1cf3080683eb6544cea85ec1085fa47d63f639

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+def countdown(seconds_midnight)
+  while seconds_midnight > 0
+    puts "#{seconds_midnight} SECOND(S)!"
+    seconds_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(sleep)
+  while countdown_with_sleep >0
+    puts "#{countdown_with_sleep} seconds(s)!"
+    sleep(1)
+    countdown_with_sleep -=1
+  end
+  "HAPPY NEW YEAR!"
 end
